ec3ba216a8e335f56ec1492e3b92743b34077694
bavely tawfik <bavlesamy@gmail.com>
2025-06-29 15:23:02 -0700

    ..

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,19 +1,30 @@
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+
 
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen does not exist.</ThemedText>
+    <View style={styles.container}>
+      <View>
         <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
+          Go to home screen!
         </Link>
-      </ThemedView>
+      </View>
+      <View>
+        <Link href="/(tabs)/explore" style={styles.link}>
+          Go to explore screen!
+        </Link>
+      </View>
+      <View>
+
+          Go to home screen!
+        
+      </View>
+    </View>
+      
     </>
   );
 }
